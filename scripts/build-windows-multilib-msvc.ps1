13c454f8c7256200f5aacea38b4f76f9fc8274c0
RyougiKukoc <st_stars@163.com>
2026-08-30 21:39:48 +0800

    ci: install NASM for MSVC multilib build

--- scripts/build-windows-multilib-msvc.ps1
+++ scripts/build-windows-multilib-msvc.ps1
@@ -7,7 +7,9 @@ $ErrorActionPreference = 'Stop'
 $SourceDir = (Resolve-Path $SourceDir).Path
 $OutputDir = [IO.Path]::GetFullPath($OutputDir)
 $Generator = 'Visual Studio 17 2022'
-$Common = @('-G', $Generator, '-A', 'x64', '-DENABLE_ASSEMBLY=ON', '-DENABLE_SHARED=OFF', '-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded')
+$Nasm = 'C:\Program Files\NASM\nasm.exe'
+if (-not (Test-Path $Nasm)) { $Nasm = (Get-Command nasm -ErrorAction Stop).Source }
+$Common = @('-G', $Generator, '-A', 'x64', '-DENABLE_ASSEMBLY=ON', "-DCMAKE_ASM_NASM_COMPILER=$Nasm", '-DENABLE_SHARED=OFF', '-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded')
 
 New-Item -ItemType Directory -Force -Path "$OutputDir\12bit", "$OutputDir\10bit", "$OutputDir\8bit" | Out-Null
 
